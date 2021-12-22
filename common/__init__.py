3c2d5c82d3f41bf79c54b280710f731e0000d19d
mas <m@stdwtr.io>
2021-12-22 15:25:36 -0500

    Add configuration module

---
+++ common/__init__.py
@@ -0,0 +1,7 @@
+from common.config import init_config
+
+conf = init_config()
+
+__all__ = [
+    'conf'
+]
---
+++ common/config.py
@@ -0,0 +1,75 @@
+import os
+from pathlib import Path
+from typing import Tuple, Any
+from dataclasses import dataclass
+
+from xdg import xdg_config_home
+
+config_root: str = os.path.join(xdg_config_home(), 'scrummy')
+config_file: str = os.path.join(config_root, '.scrummyrc')
+
+
+@dataclass
+class Config:
+    """
+    Config object for the scrummy application.
+    """
+    home: Path = Path('~/documents/scrummy').expanduser()
+
+    def __getitem__(self, key: str) -> Any:
+        return getattr(self, key)
+
+    def __setitem__(self, key: Any, value: Any):
+        if hasattr(self, key):
+            setattr(self, key, value)
+        else:
+            raise KeyError(f'{key} is not a valid configuration key.')
+
+
+def parse_line(line: str) -> Tuple[str, Any] | None:
+    """
+    Parses a line from the config file.
+
+    Parameters
+    ----------
+    line: str
+
+    Returns
+    -------
+    Tuple[str, Any]
+        The key and value of the line.
+    """
+    if line.startswith('#'):
+        return None
+    if '=' not in line:
+        return None
+    key, value = line.split('=', 1)
+    if value.startswith('"') and value.endswith('"'):
+        value = value[1:-1]
+    if '~' in value:
+        value = str(Path(value).expanduser())
+    return key, value.strip()
+
+
+def init_config() -> Config:
+    """
+    Initializes the config object.
+
+    Returns
+    -------
+    Config
+        The config object.
+    """
+    if os.path.exists(config_file):
+        with open(config_file, 'rt') as f:
+            data = f.readlines()
+    else:
+        return Config()
+    config = Config()
+    for line in data:
+        try:
+            key, val = parse_line(line)
+            config[key] = val
+        except TypeError:
+            pass
+    return config
